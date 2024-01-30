7bdcd8077f21b12102438f828f967bee0ec85554
jimkao <Jim87as0821@gmail.com>
2024-01-30 22:02:12 +0800

    修改path

--- login.js
+++ login.js
@@ -1,7 +1,6 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 const url = "https://vue3-course-api.hexschool.io/v2";
-const path = "lovecorgi";
 createApp({
   data() {
     return {
